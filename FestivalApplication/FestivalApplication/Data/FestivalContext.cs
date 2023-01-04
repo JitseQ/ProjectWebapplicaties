90a38cf429a56a522d798dcb6fd961cfa4fbd371
JitseQ <r0885690@student.thomasmore.be>
2023-01-04 19:58:42 +0100

    Databank

--- FestivalApplication/FestivalApplication/Data/FestivalContext.cs
+++ FestivalApplication/FestivalApplication/Data/FestivalContext.cs
@@ -5,5 +5,32 @@ namespace FestivalApplication.Data
 {
     public class FestivalContext : DbContext
     {
+        public FestivalContext(DbContextOptions<FestivalContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Artiest> Artiesten { get; set; }
+        public DbSet<ArtiestDatum> ArtiestDatums { get; set; }
+        public DbSet<ArtiestGenre> ArtiestGenres { get; set; }
+        public DbSet<Factuur> Facturen { get; set; }
+        public DbSet<FactuurProduct> FactuurProducten { get; set; }
+        public DbSet<Gebruiker> Gebruikers { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Product> Producten { get; set; }
+        public DbSet<Stage> Stages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Artiest>().ToTable("Artiest");
+            modelBuilder.Entity<ArtiestDatum>().ToTable("ArtiestDatum");
+            modelBuilder.Entity<ArtiestGenre>().ToTable("ArtiestGenre");
+            modelBuilder.Entity<Factuur>().ToTable("Factuur");
+            modelBuilder.Entity<FactuurProduct>().ToTable("FactuurProduct");
+            modelBuilder.Entity<Gebruiker>().ToTable("Gebruiker");
+            modelBuilder.Entity<Genre>().ToTable("Genre");
+            modelBuilder.Entity<Product>().ToTable("Product");
+            modelBuilder.Entity<Stage>().ToTable("Stage");
+        }
     }
 }
